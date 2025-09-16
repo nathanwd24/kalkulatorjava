213fde56bab66c9629bcbe95ca87616a930f359b
Nathanael Widayaka <nathanwd24@gmail.com>
2025-09-16 13:51:58 +0700

    Add files via upload

---
+++ Angka.java
@@ -0,0 +1,10 @@
+public class Angka {
+    int a;
+    int b;
+    int tambah;
+    int kali;
+    int kurang;
+    int bagi;
+
+    int pilihan;
+}
---
+++ Kalkulator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Kalkulator {
+    public static void main(String[] args) {
+        Angka angka = new Angka();
+        Scanner input = new Scanner(System.in);
+        System.out.println("============ KALKULATOR ============");
+
+        System.out.print("Masukkan angka pertama : ");
+        angka.a = input.nextInt();
+        System.out.println("Angka pertama : " + angka.a);
+
+        System.out.print("Masukkan angka kedua : ");
+        angka.b = input.nextInt();
+        System.out.println("Angka kedua : " + angka.b);
+        
+        angka.tambah = angka.a+angka.b;
+        angka.kali = angka.a*angka.b;
+        angka.kurang = angka.a-angka.b;
+        angka.bagi = angka.a/angka.b;
+
+        System.out.println("1. Tambah");
+        System.out.println("2. Kali");
+        System.out.println("3. Kurang");
+        System.out.println("4. Bagi");
+        System.out.print("Pilihan : ");
+        angka.pilihan = input.nextInt();
+        
+        switch (angka.pilihan) {
+            case 1:
+                System.out.print(angka.a + " + " + angka.b + " = " + angka.tambah);
+                break;
+            case 2:
+                System.out.print(angka.a + " * " + angka.b + " = " + angka.kali);
+                break;
+            case 3:
+                System.out.print(angka.a + " - " + angka.b + " = " + angka.kurang);
+                break;
+            case 4:
+                System.out.print(angka.a + " / " + angka.b + " = " + angka.bagi);
+                break;
+            default:
+            System.err.println("Pilihan tidak ada!");
+        }
+
+        input.close();
+    }
+}
