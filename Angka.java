a5b133aa81d9f9689627548f0eb18ac97d00b14d
Nathanael Widayaka <nathanwd24@gmail.com>
2025-09-22 09:26:14 +0700

    Kalkulator
    Angka.java now use Method Parameters and better Constructor on Kalkulator.java

--- Angka.java
+++ Angka.java
@@ -1,10 +1,17 @@
 public class Angka {
     double a;
     double b;
-    double tambah;
-    double kali;
-    double kurang;
-    double bagi;
-
     int pilihan;
-}
+    public void tambah(double x, double z) {
+        System.out.print("\n" + x + " + " + z + " = " + (x + z));
+    }
+    public void kali(double x, double z) {
+        System.out.print("\n" + x + " * " + z + " = " + (x * z));
+    }
+    public void kurang(double x, double z) {
+        System.out.print("\n" + x + " - " + z + " = " + (x - z));
+    }
+    public void bagi(double x, double z) {
+        System.out.print("\n" + x + " / " + z + " = " + (x / z));
+    }
+} // Nathanael Widayaka (2407421029) - TMJ 3B - 18/09/2025
--- Kalkulator.java
+++ Kalkulator.java
@@ -2,42 +2,38 @@ import java.util.Scanner;
 
 class Kalkulator {
     public static void main(String[] args) {
-        Angka angka = new Angka();
+        Angka number = new Angka();
         Scanner input = new Scanner(System.in);
         System.out.println("============ KALKULATOR ============");
 
         System.out.print("Masukkan angka pertama : ");
-        angka.a = input.nextDouble();
-        System.out.println("Angka pertama : " + angka.a);
+        number.a = input.nextDouble();
+        System.out.println("Angka pertama : " + number.a + "\n");
 
         System.out.print("Masukkan angka kedua : ");
-        angka.b = input.nextDouble();
-        System.out.println("Angka kedua : " + angka.b);
-        
-        angka.tambah = angka.a+angka.b;
-        angka.kali = angka.a*angka.b;
-        angka.kurang = angka.a-angka.b;
-        angka.bagi = angka.a/angka.b;
+        number.b = input.nextDouble();
+        System.out.println("Angka kedua : " + number.b + "\n");
 
+        System.out.println("Pilih operasi perhitungan");
         System.out.println("1. Tambah");
         System.out.println("2. Kali");
         System.out.println("3. Kurang");
         System.out.println("4. Bagi");
         System.out.print("Pilihan : ");
-        angka.pilihan = input.nextInt();
+        number.pilihan = input.nextInt();
         
-        switch (angka.pilihan) {
+        switch (number.pilihan) {
             case 1:
-                System.out.print(angka.a + " + " + angka.b + " = " + angka.tambah);
+                number.tambah(number.a, number.b);
                 break;
             case 2:
-                System.out.print(angka.a + " * " + angka.b + " = " + angka.kali);
+                number.kali(number.a, number.b);
                 break;
             case 3:
-                System.out.print(angka.a + " - " + angka.b + " = " + angka.kurang);
+                number.kurang(number.a, number.b);
                 break;
             case 4:
-                System.out.print(angka.a + " / " + angka.b + " = " + angka.bagi);
+                number.bagi(number.a, number.b);
                 break;
             default:
             System.err.println("Pilihan tidak ada!");
@@ -45,4 +41,4 @@ class Kalkulator {
 
         input.close();
     }
-}
+} // Nathanael Widayaka (2407421029) - TMJ 3B - 18/09/2025
